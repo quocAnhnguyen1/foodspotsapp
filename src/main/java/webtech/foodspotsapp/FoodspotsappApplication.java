7fddd7281117467de75152dfc86b74ba9b7a525a
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2025-04-20 14:24:39 +0200

    add README

---
+++ src/main/java/webtech/foodspotsapp/FoodspotsappApplication.java
@@ -0,0 +1,13 @@
+package webtech.foodspotsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodspotsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodspotsappApplication.class, args);
+	}
+
+}
