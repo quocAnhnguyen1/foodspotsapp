bef700efd051f3e1283b7e1ef861b24383fa5078
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2025-06-12 21:48:49 +0200

    add dockerfile

--- src/main/java/webtech/foodspotsapp/config/CorsConfig.java
+++ src/main/java/webtech/foodspotsapp/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("https://foodspotsapp-backend.onrender.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*");
       }
